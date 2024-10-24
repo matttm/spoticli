d650d41d8967a2b80669599654cc0eec9a363914
matttm <matttmaloney@gmail.com>
2024-10-24 13:53:37 -0400

    adding docs

--- spoticli-cli/internal/utilities/http.go
+++ spoticli-cli/internal/utilities/http.go
@@ -12,6 +12,15 @@ import (
 	"github.com/matttm/spoticli/spoticli-cli/internal/models"
 )
 
+// GetBytesOpenReader gets an item from the backend and returns
+// the responsre body as a fresh reader
+//
+// headerCb is a function taking an *http.Request that customies it
+//
+// seg an AudioSegment that will be updated with the response'
+// "Content-Range" header, nost useful for partial data
+//
+// args is an arbitrary number of items to be used in string formatting
 func GetBytesOpenReader(headerCb func(*http.Request), seg *models.AudioSegment, args ...interface{}) io.ReadCloser {
 	var t interface{} = config.SERVER_URL
 	slice := make([]interface{}, 1)
@@ -41,6 +50,16 @@ func GetBytesOpenReader(headerCb func(*http.Request), seg *models.AudioSegment, 
 	}
 	return res.Body
 }
+
+// GetBytesOpenReader gets an item from the backend and returns
+// the response body as a []byte
+//
+// headerCb is a function taking an *http.Request that customies it
+//
+// seg an AudioSegment that will be updated with the response'
+// "Content-Range" header, nost useful for partial data
+//
+// args is an arbitrary number of items to be used in string formatting
 func GetBytesBackend(headerCb func(*http.Request), seg *models.AudioSegment, args ...interface{}) ([]byte, error) {
 	body := GetBytesOpenReader(headerCb, seg, args...)
 	data, err := io.ReadAll(body)
@@ -50,6 +69,17 @@ func GetBytesBackend(headerCb func(*http.Request), seg *models.AudioSegment, arg
 	defer body.Close()
 	return data, nil
 }
+
+// GetBufferedAudioSegment gets a segment of queried song and
+// returns the data as a streamer
+//
+// header
+//
+// id of audio track
+//
+// seg an AudioSegment that will be updated with the response'
+// "Content-Range" header, nost useful for partial data
+
 func GetBufferedAudioSegment(header []byte, id string, seg *models.AudioSegment) ([]byte, beep.StreamSeekCloser, beep.Format) {
 	data, _ := GetBytesBackend(
 		func(r *http.Request) {
