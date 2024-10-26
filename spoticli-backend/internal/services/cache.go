feec74f33c31ecfa62fc0171adfa00ba6b8dac3e
matttm <matttmaloney@gmail.com>
2024-10-26 10:14:55 -0400

    finished cache

--- spoticli-backend/internal/services/cache.go
+++ spoticli-backend/internal/services/cache.go
@@ -2,7 +2,7 @@
 package services
 
 type CacheService struct {
-	redis map[int]map[int64][]byte // map of id to song, which is cut in segments
+	Redis map[int][][]byte // map of id to song, which is cut in segments
 }
 
 var cacheService *CacheService
@@ -15,10 +15,29 @@ func getCacheService() *CacheService {
 }
 
 func isItemCached(id int) bool {
+	_, ok := getCacheService().Redis[id]
+	return ok
 }
 
 func getSegmentFromCache(id, segment int) []byte {
+	return getCacheService().Redis[id][segment]
 }
 
-func cacheItem(id int, data [][]byte) error {
+func cacheItem(id int, frames [][]byte) error {
+	if isItemCached(id) {
+		panic("Item is already cached")
+	}
+	frameClusterSize := GetConfigValue[int]("FRAME_CLUSTER_SIZE")
+	cur := 0
+	end := 0
+	n := len(frames)
+	var songSlices [][]byte
+	for cur < n {
+		end = min(cur+frameClusterSize, n)
+		b := flatten(frames[cur:end])
+		songSlices = append(songSlices, b)
+		cur += frameClusterSize
+	}
+	getCacheService().Redis[id] = songSlices
+	return nil
 }
