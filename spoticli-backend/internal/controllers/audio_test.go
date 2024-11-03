1137e119218e9ba0586254491de27d90dbc4adaa
matttm <matttmaloney@gmail.com>
2024-11-02 21:32:53 -0400

    wrote basoc controller test

--- spoticli-backend/internal/controllers/audio_test.go
+++ spoticli-backend/internal/controllers/audio_test.go
@@ -1,19 +1,21 @@
 package controllers
 
 import (
-	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gorilla/mux"
 	mock_services "github.com/matttm/spoticli/spoticli-backend/internal/services/mocks"
+	"github.com/stretchr/testify/assert"
 	"go.uber.org/mock/gomock"
 )
 
 const presignedUrl = "aws.s3/resource"
 
-func TestAudioController_GetPresignedUrl(t *testing.T) {
+func TestAudioController_GetPresignedUrl_Success(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
@@ -33,12 +35,81 @@ func TestAudioController_GetPresignedUrl(t *testing.T) {
 	defer server.Close()
 
 	handler.ServeHTTP(w, req) // Assert on the status code
-	if w.Code != http.StatusOK {
-		t.Errorf("Expected status code 200, got %d", w.Code)
-	}
+	assert.Equalf(t, w.Code, http.StatusOK, "Expected status code 200, got %d", w.Code)
 
 	// Parse the response body (if needed)
-	var responseBody []byte
-	json.NewDecoder(w.Body).Decode(&responseBody)
+	var got = w.Body.String()
+	assert.Equalf(t, presignedUrl, got, "Expected presigned url %s, got %s", presignedUrl, got)
+}
+func TestAudioController_GetAudio_Success(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
+	svc := mock_services.NewMockApiAudioService(ctrl)
+	audioService = svc
+
+	// Create a new request recorder
+	w := httptest.NewRecorder()
+	// Create a mock request
+	audio := "Some fake audio data"
+	b := []byte(audio)
+	req := httptest.NewRequest(http.MethodGet, "/audio/proxy/1", nil)
+	length := int64(len(b))
+	svc.EXPECT().GetAudio(1).Return(b, &length, nil).Times(1)
+
+	// Create a new test server
+	handler := mux.NewRouter()
+	handler.Path("/audio/proxy/{id:[0-9]+}").Methods(http.MethodGet).HandlerFunc(GetAudio)
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
+	handler.ServeHTTP(w, req) // Assert on the status code
+	assert.Equalf(t, w.Code, http.StatusOK, "Expected status code 200, got %d", w.Code)
+
+	// Parse the response body (if needed)
+	data, _ := io.ReadAll(w.Body)
+	body := string(data)
+	h := w.Header()
+	assert.Equal(t, "audio/mp3", h.Get("Content-Type"), "Expected Content-Type of 'audio/mp3'")
+	assert.Equal(t, fmt.Sprint(length), h.Get("Content-Length"), "Expected Content-Length of bleh")
+	assert.Equal(t, audio, body, "Expected")
+}
+func TestAudioController_GetAudioSegment_Success(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	svc := mock_services.NewMockApiAudioService(ctrl)
+	audioService = svc
+
+	// Create a new request recorder
+	w := httptest.NewRecorder()
+	// Create a mock request
+	audio := "Some fake audio data"
+	b := []byte(audio)
+	var start int64 = 0
+	var end int64 = 0
+	_range := fmt.Sprintf("bytes=%d-%d", start, end)
+	part := b[:1]
+	length := len(part)
+	req := httptest.NewRequest(http.MethodGet, "/audio/proxy/stream/1", nil)
+	filesize := int64(len(b))
+	svc.EXPECT().StreamAudioSegment(1, &start, &end).Return(part, &length, &filesize, nil).Times(1)
+
+	// Create a new test server
+	handler := mux.NewRouter()
+	handler.Path("/audio/proxy/stream/{id:[0-9]+}").Methods(http.MethodGet).HandlerFunc(GetAudioPart)
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
+	// setting range
+	req.Header.Add("Range", _range)
+	handler.ServeHTTP(w, req) // Assert on the status code
+	assert.Equalf(t, w.Code, http.StatusPartialContent, "Expected status code 200, got %d", w.Code)
+
+	// Parse the response body (if needed)
+	data, _ := io.ReadAll(w.Body)
+	h := w.Header()
+	assert.Equal(t, "audio/mp3", h.Get("Content-Type"), "Expected Content-Type of 'audio/mp3'")
+	assert.Equal(t, fmt.Sprint(length), h.Get("Content-Length"), "Expected Content-Length of bleh")
+	assert.Equal(t, string(data), "S", "Expected []byte with content x: got y")
 }
--- spoticli-backend/internal/services/audio_wrap.go
+++ spoticli-backend/internal/services/audio_wrap.go
@@ -5,12 +5,12 @@ type AudioServiceWrap struct{}
 // GetPresignedUrl gets a presigned url
 // for downloading an object from s3
 func (s *AudioServiceWrap) GetPresignedUrl(id int) (string, error) {
-	return s.GetPresignedUrl(id)
+	return GetPresignedUrl(id)
 }
 
 func (s *AudioServiceWrap) GetAudio(id int) ([]byte, *int64, error) {
-	return s.GetAudio(id)
+	return GetAudio(id)
 }
 func (s *AudioServiceWrap) StreamAudioSegment(id int, start, end *int64) ([]byte, *int, *int64, error) {
-	return s.StreamAudioSegment(id, start, end)
+	return StreamAudioSegment(id, start, end)
 }
