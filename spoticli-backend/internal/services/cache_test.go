c4d6319e7bf3232d2d3f8a33390c554760db5e0f
matttm <matttmaloney@gmail.com>
2024-11-04 08:52:11 -0500

    working on caching test

--- spoticli-backend/internal/services/cache_test.go
+++ spoticli-backend/internal/services/cache_test.go
@@ -10,6 +10,11 @@ import (
 
 const TEST_KEY = "test"
 
+type cacheServiceTestCase struct {
+	clusterSize int
+	assertFn    func(t *testing.T, frame [][]byte)
+}
+
 func Test_CacheService_CacheItem_Success(t *testing.T) {
 	cs := getCacheService()
 	data := [][]byte{
@@ -29,23 +34,35 @@ func Test_CacheService_CacheItem_Success(t *testing.T) {
 		[]byte("frame:E"),
 		[]byte("frame:F"),
 	}
+	_table := []cacheServiceTestCase{
+		{
+			clusterSize: 2,
+			assertFn: func(t *testing.T, res [][]byte) {
+				assert.Equal(t, 8, len(res), "Expected %d frame clusters: got %d", 8, len(res))
+				assert.Equal(t, []byte("frame:oneframe:two"), res[0], "Expected")
+				assert.Equal(t, []byte("frame:threeframe:four"), res[1], "Expected")
+				assert.Equal(t, []byte("frame:fiveframe:six"), res[2], "Expected")
+				assert.Equal(t, []byte("frame:sevenframe:eight"), res[3], "Expected")
+				assert.Equal(t, []byte("frame:nineframe:A"), res[4], "Expected")
+				assert.Equal(t, []byte("frame:Bframe:C"), res[5], "Expected")
+				assert.Equal(t, []byte("frame:Dframe:E"), res[6], "Expected")
+				assert.Equal(t, []byte("frame:F"), res[7], "Expected")
+
+			},
+		},
+	}
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 	// inject mock
 	configSvc := mock_services.NewMockConfigServiceApi(ctrl)
 	cs.configService = configSvc
-	configSvc.EXPECT().GetConfigValueInt64("FRAME_CLUSTER_SIZE").Return(int64(2)).AnyTimes()
-	var channel chan []byte
-	_ = cacheItem(TEST_KEY, data, int64(4), int64(0), channel)
-	res := cs.Redis[TEST_KEY]
-	assert.Equal(t, 8, len(res), "Expected %d frame clusters: got %d", 8, len(res))
-	assert.Equal(t, []byte("frame:oneframe:two"), res[0], "Expected")
-	assert.Equal(t, []byte("frame:threeframe:four"), res[1], "Expected")
-	assert.Equal(t, []byte("frame:fiveframe:six"), res[2], "Expected")
-	assert.Equal(t, []byte("frame:sevenframe:eight"), res[3], "Expected")
-	assert.Equal(t, []byte("frame:nineframe:A"), res[4], "Expected")
-	assert.Equal(t, []byte("frame:Bframe:C"), res[5], "Expected")
-	assert.Equal(t, []byte("frame:Dframe:E"), res[6], "Expected")
-	assert.Equal(t, []byte("frame:F"), res[7], "Expected")
+	for _, v := range _table {
+		delete(cs.Redis, TEST_KEY)
+		configSvc.EXPECT().GetConfigValueInt64("FRAME_CLUSTER_SIZE").Return(int64(v.clusterSize)).Times(1)
+		var channel chan []byte
+		_ = cacheItem(TEST_KEY, data, int64(4), int64(0), channel)
+		res := cs.Redis[TEST_KEY]
+		v.assertFn(t, res)
+	}
 
 }
