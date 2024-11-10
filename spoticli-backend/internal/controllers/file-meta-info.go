bb3e903bc70032b1c4ed2bf701ad17ca4d28a709
matttm <matttmaloney@gmail.com>
2024-11-09 20:08:56 -0500

    working on file router

--- spoticli-backend/internal/controllers/file-meta-info.go
+++ spoticli-backend/internal/controllers/file-meta-info.go
@@ -1,6 +1,17 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/matttm/spoticli/spoticli-backend/internal/services"
+)
 
 func GetAllFilesOfType(w http.ResponseWriter, r *http.Request) {
+	files := services.GetAllFilesOfType(1)
+	b, err := json.Marshal(files)
+	if err != nil {
+		panic(err)
+	}
+	w.Write(b)
 }
